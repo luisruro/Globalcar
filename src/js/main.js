26e6fbb8632760cc820b1c76b51c729057708ce9
luisruro <lerubioromero@gmail.com>
2024-05-06 22:08:47 -0500

    Se finaliza hasta la primera sección.

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+// Import our custom CSS
+import '../scss/custom.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+// All animations will take twice the time to accomplish
+document.documentElement.style.setProperty('--animate-duration', '4s');
+
+// All animations will take half the time to accomplish
+document.documentElement.style.setProperty('--animate-duration', '.9s');
