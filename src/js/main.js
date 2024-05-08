a22455157a410b91f24afed39cdd68d546635f6e
luisruro <lerubioromero@gmail.com>
2024-05-07 23:29:37 -0500

    Se finaliza el proyecto, falta el responsive

--- src/js/main.js
+++ src/js/main.js
@@ -9,3 +9,9 @@ document.documentElement.style.setProperty('--animate-duration', '4s');
 
 // All animations will take half the time to accomplish
 document.documentElement.style.setProperty('--animate-duration', '.9s');
+
+
+window.addEventListener('load', () => {
+    const currentDate = new Date();
+    currentYear.innerText = currentDate.getFullYear();
+});
